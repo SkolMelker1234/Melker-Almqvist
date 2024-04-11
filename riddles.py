3ae5884500bc4020db3e4571f89c024c4a36375d
SkolMelker1234 <151731699+SkolMelker1234@users.noreply.github.com>
2024-04-11 10:40:14 +0200

    riddles.py

--- riddles.py
+++ riddles.py
@@ -10,13 +10,13 @@ answer3 = ["footsteps", "footstep"]
 riddle4 = "What’s a knight’s favorite type of music?"
 answer4 = ["metal", "heavy metal"]
 riddle5 = "Why did the blacksmith have trouble sleeping at night?"
-answer5 = ["knightmares", "knight mares", "knight-mares"]
+answer5 = ["knightmares", "knight mares"]
 riddle6 = """Oft I must strive with wind and wave, Battle them both when under the sea
 I feel out the bottom, a foreign land. In lying still I am strong in the strife;
 If I fail in that they are stronger than I, And wrenching me loose, soon put me to rout.
 They wish to capture what I must keep. I can master them both if my grip holds out,
 If the rocks bring succor and lend support, Strength in the struggle. Ask me my name!"""
-answer6 = "anchor"
+answer6 = ["anchor", "an anchor"]
 
 riddle_list = {
     riddle1 : answer1,
@@ -27,7 +27,20 @@ riddle_list = {
     riddle6 : answer6
 }
 
+
 def riddle():
-    choice = rand.randint(0, 5)
-    riddle = riddle_list.get(choice)
-    print(riddle)
+    rid, key = rand.choice(list(riddle_list.items()))
+    return rid, key
+
+def check_answ(key):
+    i = 0
+    while i <=1:
+        inp = input("->").lower()
+        if inp == key[0] or inp == key[1]:
+            return "correct"
+        elif i < 1:
+            print("Try again")
+            i += 1
+        else:
+            i += 1   
+    return "incorrect"
