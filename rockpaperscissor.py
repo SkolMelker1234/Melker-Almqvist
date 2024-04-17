8d6a04cc8b1499f75a9df1224001dda180a209d2
SkolMelker1234 <151731699+SkolMelker1234@users.noreply.github.com>
2024-04-17 11:57:41 +0200

    rockpaperscissor.py

---
+++ rockpaperscissor.py
@@ -0,0 +1,38 @@
+import random as rand
+
+rps_lst = ["r", "p", "s"]
+def play():
+    while True:
+        inp = input(f"Rock(r), Paper(p) or Scissors(s)?\n")
+        enemy = rand.choice(rps_lst)
+        if inp.lower() == "r":
+            if enemy == "s":
+                print("You win")
+                return "You win"
+            elif enemy == "r":
+                print("play again")
+                continue
+            else:
+                print("You lost")
+                return "You lost"
+        
+        elif inp.lower() == "p":
+            if enemy == "s":
+                print("You lost")
+                return "You lost"
+            elif enemy == "r":
+                print("play win")
+                return "You win"
+            else:
+                print("Play again")
+                continue
+        else:
+            if enemy == "s":
+                print("Play again")
+                continue
+            elif enemy == "r":
+                print("You lost")
+                return "You lost"
+            else:
+                print("You win")
+                return "You win"
