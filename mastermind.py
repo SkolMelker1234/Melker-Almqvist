96758370c13336d43045729257c6808be90c3b8f
SkolMelker1234 <151731699+SkolMelker1234@users.noreply.github.com>
2024-05-15 11:04:40 +0200

    mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,4 @@
-import random as rand
-
+from random import randint
 class Colors: 
     """
     \u001b är en ANSI escape code och innehåller färger och textstilar ex fetstil. 
@@ -30,26 +29,20 @@ def numbers(guess):
     return True
 
 
-def inputcheck(alternativ_1, alternativ_2, intake):
-    return intake.lower() == alternativ_1 or intake.lower() == alternativ_2
-
 def hard(): #Klar
-    return [str(rand.randint(1, 6)) for _ in range(4)]
+    return [str(randint(1, 6)) for _ in range(4)]
 
 
 def difficulty():#Klar
-    while True:
-        val = "svår"
-        if val.lower().replace(" ", "") == "svår":
-            return hard()
-        #Ifall inmatningen inte motsvarar någon av nummrerna så kommer den skriva ut fel inmatning med röd text.
+    return hard()
+    #Ifall inmatningen inte motsvarar någon av nummrerna så kommer den skriva ut fel inmatning med röd text.
 
 def gui(guesses, feedback):
     print(f"\n  {Colors.title + Colors.bold}Moves #         move          Feedback{Colors.endc}")
     print(f"{Colors.border + Colors.bold}------------------------------------------{Colors.endc}")
-    for i in range(12):
-        print(f"\n{Colors.text}{(12 - i):5.0f}         {guesses[12 - (i + 1)]}         {feedback[12 - (i + 1)]}{Colors.endc}")
-    #Skriver ut talen från 12 till 1 och sen feedbacken för just den omgången anropas och skrivs ut
+    for i in range(8):
+        print(f"\n{Colors.text}{(8 - i):5.0f}         {guesses[8 - (i + 1)]}         {feedback[8 - (i + 1)]}{Colors.endc}")
+    #Skriver ut talen från 8 till 1 och sen feedbacken för just den omgången anropas och skrivs ut
     print(f"\n{Colors.border + Colors.bold}------------------------------------------{Colors.endc}")
 
 def intake(amount, guesses):
@@ -59,7 +52,7 @@ def intake(amount, guesses):
         #här kollar den om nummrerna i input spel_giss faktiskt är mellan 1-6 samt om de är nummer.
         if len(player_guess) != 4:
             print(f"{Colors.error + Colors.bold}\nHas to be four numbers{Colors.endc}")
-
+        
         elif correct_numbers is True:
             guesses[amount] = player_guess.replace("", " ")
             return player_guess
@@ -68,19 +61,19 @@ def intake(amount, guesses):
             print(f"{Colors.error + Colors.bold}\nHas to be numbers between 1 and 6{Colors.endc}")
 
 def guess():
-    guesses = ["", "", "", "", "", "", "", "", "", "", "", ""]
-    feedback = ["", "", "", "", "", "", "", "", "", "", "", ""]
+    guesses = ["", "", "", "", "", "", "", ""]
+    feedback = ["", "", "", "", "", "", "", ""]
     amount_guesses = 0
     combination = difficulty()
     #Här anropas funktionen difficulty så vi får en kombination man ska försöka gissa sig till.
     while True:
         removed = 0
-
+        
         resultat = []
         combination_copy = combination.copy()
         #listan combination_copy får alla kombinationens nummer i sig dvs blir kopierad
 
-        if amount_guesses == 12:
+        if amount_guesses == 8:
             print(f"No more guesses!\n Correct combination: {' '.join(combination)}")
             break
         #Här skrivs listan kombination ut som ett kombinerat nummer med mellanrum.
@@ -92,13 +85,13 @@ def guess():
                 resultat.append("✔")
                 combination_copy.pop(i - removed)
                 removed += 1
-
+            
 
         for i in range(len(player_guess)):
             if player_guess[i] in combination_copy and player_guess[i] != combination[i]:
                 resultat.append("❐")
                 combination_copy.remove(player_guess[i])
-
+               
 
         feedback[amount_guesses] = " ".join(resultat)
 
@@ -111,17 +104,17 @@ def guess():
         #ifall man skrev rätt så avslutas hela funktionen
 
         amount_guesses += 1
-        #man kan bara gissa 12 gånger sen är spelet över
+        #man kan bara gissa 8 gånger sen är spelet över
 
 def info():
     kört_innan = input("Do you require information (yes/no) -> ")
-    while not inputcheck("yes", "no", kört_innan):
+    while kört_innan not in ["yes", "no", "y", "n"]:
         #När inputcheck() returnerar True så avbryts loopen
-        if inputcheck("yes", "no", kört_innan) is False:
+        if kört_innan not in ["yes", "no", "y", "n"]:
             print(f"\n{Colors.error + Colors.bold}Wrong input!{Colors.endc}")
         #Om det man har skrivit in inte är 'ja' eller 'nej' så kommer koden säga att det är fel inmatning
-        kört_innan = input("\nJa eller nej -> ")
-    return kört_innan.lower().replace(" ", "") == "ja"
+        kört_innan = input("\nYes or no -> ")
+    return True
 
 
 
@@ -129,7 +122,7 @@ def play():
     if info() is True:
         print(f'''{Colors.explanation} 
         Puzzlemaster is going to randomize a code between 1 and 6.
-        Your goal is to guess the code in 12 guesses
+        Your goal is to guess the code in 8 guesses
         After each guess you will get a response:
         For every correct number in the right place: ✔
         For every correct number in the wrong place: ❐
